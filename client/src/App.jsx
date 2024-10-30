8f1f113e39c5468e46be48c17e61bbb8f7cb0c22
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-30 09:39:54 +0100

    creation build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './components/routes/AppRoutes';
 import Navbar from './components/organisms/navbar/navbar';
 import './components/styles/global.css';
